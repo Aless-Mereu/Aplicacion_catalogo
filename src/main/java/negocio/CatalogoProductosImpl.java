02d58379bc8641069ee2ccabb2fcc3fd672c17c6
AlessandroGarcia <alessandro.garcia234@comunidadunir.net>
2025-03-10 19:38:55 +0100

    Implementando metodos y documentando en la clase CatalogoProductosImpl.

--- src/main/java/negocio/CatalogoProductosImpl.java
+++ src/main/java/negocio/CatalogoProductosImpl.java
@@ -4,25 +4,37 @@ import javabean.Familia;
 import javabean.Producto;
 import javabean.Proveedor;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * Clase sin atributos que implementa los metodos de ICatalogo.
+ * Se crea un ArrayList con los objetos.
+ */
 
 public class CatalogoProductosImpl implements Icatalogo {
     private ArrayList<Producto> lista;
 
     public CatalogoProductosImpl() {
-        lista =new ArrayList<Producto>();
+        lista = new ArrayList<Producto>();
         cargarDatos();
     }
-    private void cargarDatos() {
-        Familia familia = new Familia("Bebidas",1);
-        Proveedor proveedor = new Proveedor("100","Bodegas Ruiz","bebidas","Ruiz@gmail.com","Spain");
 
-        lista.add(new Producto(1, "Camiseta", "Camiseta negra hombre talla M", "Nike",  40.00, familia , proveedor));
-        lista.add(new Producto(2, "Pantalón", "Pantalón de Hombre negro", "Levi's", 60.00,familia , proveedor));
-        lista.add(new Producto(3, "Zapatillas", "Zapatillas deportivas de Mujer talla 37", "Adidas", 80.00,familia , proveedor));
-        lista.add(new Producto(4, "Sudadera", "Sudadera con capucha de mujer", "Puma", 55.00,familia , proveedor));
-        lista.add(new Producto(5, "Chaqueta", "Chaqueta de cuero de mujer talla M", "Zara", 120.00, familia , proveedor));
+    /**
+     * Metodo que carga una lista con los objetos creados
+     */
+    private void cargarDatos() {
+        Familia familia = new Familia("Normal", 1);
+        Familia familia2 = new Familia("Lujo", 2);
+        Familia familia3 = new Familia("Superlujo", 3);
+        Proveedor proveedor = new Proveedor("100", "Amparo Larrañaga", "Textil", "Amparo@gmail.com", "Spain");
+
+        lista.add(new Producto(1, "Camiseta", "Camiseta negra hombre talla M", "Nike", 40.00, familia, proveedor));
+        lista.add(new Producto(2, "Pantalón", "Pantalón de Hombre negro", "Levi's", 60.00, familia3, proveedor));
+        lista.add(new Producto(3, "Zapatillas", "Zapatillas deportivas de Mujer talla 37", "Adidas", 80.00, familia, proveedor));
+        lista.add(new Producto(4, "Sudadera", "Sudadera con capucha de mujer", "Puma", 55.00, familia2, proveedor));
+        lista.add(new Producto(5, "Chaqueta", "Chaqueta de cuero de mujer talla M", "Zara", 120.00, familia3, proveedor));
 
     }
 
@@ -34,7 +46,11 @@ public class CatalogoProductosImpl implements Icatalogo {
      */
     @Override
     public boolean altaProducto(Producto producto) {
-        return false;
+        if (lista.contains(producto))
+            return false;
+        lista.add(producto);
+        return true;
+
     }
 
     /**
@@ -44,7 +60,10 @@ public class CatalogoProductosImpl implements Icatalogo {
      */
     @Override
     public boolean eliminaProducto(long idProducto) {
+        if (lista.contains(idProducto))
+            lista.remove(idProducto);
         return false;
+
     }
 
     /**
@@ -54,7 +73,10 @@ public class CatalogoProductosImpl implements Icatalogo {
      */
     @Override
     public boolean modificaProducto(Producto producto) {
+        if (lista.contains(producto))
+            return true;
         return false;
+
     }
 
     /**
@@ -64,6 +86,9 @@ public class CatalogoProductosImpl implements Icatalogo {
      */
     @Override
     public Producto buscarUno(long idProducto) {
+        if (lista.contains(idProducto)) {
+            Producto producto = lista.get(lista.indexOf(idProducto));
+        }
         return null;
     }
 
@@ -72,7 +97,7 @@ public class CatalogoProductosImpl implements Icatalogo {
      */
     @Override
     public List<Producto> buscarTodos() {
-        return List.of();
+        return lista;
     }
 
     /**
@@ -82,7 +107,14 @@ public class CatalogoProductosImpl implements Icatalogo {
      */
     @Override
     public List<Producto> buscarPorFamilia(int idFamilia) {
-        return List.of();
+        List<Producto> aux = new ArrayList<>();
+        for (Producto producto : lista) {
+            if (producto.getFamilia().getIdFamilia() == idFamilia) {
+                aux.add(producto);
+                return null;
+            }
+        }
+        return aux;
     }
 
     /**
@@ -114,4 +146,5 @@ public class CatalogoProductosImpl implements Icatalogo {
     public List<Producto> buscarPorProveedor(String cif) {
         return List.of();
     }
+
 }
