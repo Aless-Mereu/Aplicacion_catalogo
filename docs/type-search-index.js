f535608432a9740c8c03b671c656ec715465a04e
AlessandroGarcia <alessandro.garcia234@comunidadunir.net>
2025-03-16 10:53:21 +0100

    Documentacion JavaDoc creada

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"negocio","l":"CatalogoProductosImpl"},{"p":"javabean","l":"Familia"},{"p":"negocio","l":"Icatalogo"},{"p":"org.example","l":"Main"},{"p":"javabean","l":"Producto"},{"p":"javabean","l":"Proveedor"}];updateSearchResults();
